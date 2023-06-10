507ec832900a6f85df07876a1778f570699e17c8
LiveInGround <129398571+LiveInGround@users.noreply.github.com>
2023-06-10 14:21:46 +0200

    Update main.py

--- files/main.py
+++ files/main.py
@@ -1,10 +1,16 @@
 """Main file for the project"""
 
 #Internal imports
-import builder.lib_installer
+import builder.lib_installer as inst
+
+installer = inst.LibInstaller()
 
 #Externals exports
 try:
     import os
 except ModuleNotFoundError:
     raise RuntimeError
+try:
+    import tkinter
+except ModuleNotFoundError:
+    installer.install("tkinter")
